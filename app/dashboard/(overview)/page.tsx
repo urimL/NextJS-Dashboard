15adf2d3688529ef6ca6d6f737ff1f3b1dd750a9
urimL <toulim0525@gmail.com>
2026-01-21 12:46:17 +0900

    chapter15 수정

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,12 @@ import { fetchLatestInvoices, fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
     const latestInvoices = await fetchLatestInvoices();
     const {
